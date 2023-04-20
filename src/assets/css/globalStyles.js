0ec45438fbec0398a181acdb14f552e81d198069
Flavio Andrade <94andrade.flavio@gmail.com>
2023-04-19 22:25:03 -0300

    Adicionado algumas informações ao modal de informações de pokémon

--- src/assets/css/globalStyles.js
+++ src/assets/css/globalStyles.js
@@ -28,6 +28,44 @@ const GlobalStyle = createGlobalStyle`
 
         h2 { margin-bottom: 0; }
     }
+
+    .modal-overlay {
+        background-color: rgba(0,0,0, .5);
+        backdrop-filter: blur(20px);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 999;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .main-modal-content {
+            width: 100%;
+            max-width: 40rem;
+            padding: 2rem;
+            margin: 1rem;
+            background: ${props => props.theme.main.color.white};
+            outline: none;
+            border-radius: 1rem;
+
+            display: flex;
+            align-items: center;
+            gap: 2rem;
+
+            img {
+                height: 10rem;
+            }
+
+            h2 {  
+                font-size: ${props => props.theme.main.text.title};
+                text-transform: capitalize;
+            }
+        }
+    }
 `
 
 export const App = styled.main`
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,22 +2,30 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 
 import * as S from './styles'
+import { getPokemonByName, getPokemonSpecie } from '../../api';
+import { translateText } from '../../helpers/translate';
 
 Modal.setAppElement('#root');
 
 const Card = ({ item }) => {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [specs, setSpecs] = useState({})
+  const [description, setDescription] = useState()
   const imgPath = `${ item.id }.gif`
 
   const relativePath = item.id <= 500 ? `src/assets/img/pokemonGif/${imgPath}` : item.img
 
-  function openModal() {
+  const openModal = async () => {
+    const data = await getPokemonByName(item.name)
+    const dataDescription = await getPokemonSpecie(item.name).then(res => translateText(res.flavor_text_entries[0].flavor_text))
+    setSpecs(data)
+    setDescription(dataDescription)
     setIsOpen(true);
   }
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    console.log('abriu')
+    console.log(specs)
   }
 
   function closeModal() {
@@ -25,23 +33,36 @@ const Card = ({ item }) => {
   }
 
   return (
-    <S.CardContainer>
+    <>
       <S.Card id={ item.id } color={ item.specieInfo.color } onClick={openModal}>
           <img src={ relativePath } />
           <h3>{ item.name }</h3>
       </S.Card>
       <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
-        overlayClassName="modal-overlay"
-        className="main-modal-content"
-      >
-        <img src={ relativePath } />
-        <h2>{ item.name }</h2>
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          overlayClassName="modal-overlay"
+          className="main-modal-content"
+        >
+        { modalIsOpen && (
+          <>
+            <img src={ relativePath } />
+            <div className="text-area">
+              <h2>{ item.name }</h2>
+              <div className="types">
+                <p>{ description }</p>
+                <h6>Type:</h6>
+                { specs.types.map(type => {
+                  return <p key={ type.type.name }>{ type.type.name }</p>
+                }) }
+              </div>
+            </div>
+          </>
+        ) }
       </Modal>
-    </S.CardContainer>
+    </>
   )
 }
 
